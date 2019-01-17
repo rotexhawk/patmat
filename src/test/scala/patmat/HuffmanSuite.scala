fea2d9bc50b9e94df6ee42670001c3cd9b7f63e8
Yasin Yaqoobi <yasinyaqoobi@gmail.com>
2019-01-17 17:53:41 -0500

    upto singelton

---
+++ src/test/scala/patmat/HuffmanSuite.scala
@@ -0,0 +1,92 @@
+package patmat
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import patmat.Huffman._
+
+@RunWith(classOf[JUnitRunner])
+class HuffmanSuite extends FunSuite {
+	trait TestTrees {
+		val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
+		val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
+    val charList = List('a', 'b', 'c')
+    val dupCharList = List('a', 'b', 'c', 'a')
+    val complicatedList = List('a', 'b', 'c', 'a', 'd', 'b', 'z')
+	}
+
+
+  test("weight of a larger tree") {
+    new TestTrees {
+      assert(weight(t1) === 5)
+    }
+  }
+
+  test("weight of even larger tree"){
+    new TestTrees {
+      assert(weight(t2) === 9)
+    }
+  }
+
+  test("Test times of (a, b, c)"){
+    new TestTrees {
+      assert(times(charList) === List(('c', 1), ('b', 1), ('a', 1)))
+    }
+  }
+
+  test("Test times of (a, b, c, a)"){
+    new TestTrees {
+      assert(times(dupCharList) === List(('a', 2), ('c', 1), ('b', 1)))
+    }
+  }
+
+  test("Test times of ('a', 'b', 'c', 'a', 'd', 'b', 'z')"){
+    new TestTrees {
+      assert(times(complicatedList) === List(('z', 1), ('b', 2), ('d', 1), ('a', 2), ('c',1)))
+    }
+  }
+
+  test("chars of a larger tree") {
+    new TestTrees {
+      assert(chars(t2) === List('a','b','d'))
+    }
+  }
+
+
+  test("string2chars(\"hello, world\")") {
+    assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
+  }
+
+
+  test("makeOrderedLeafList for some frequency table") {
+    assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
+  }
+
+  test("Singleton") {
+    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
+    val t2 = Fork(Leaf('c',2), Leaf('d',3), List('c','d'), 5)
+
+    assert(singleton(List(t1)).equals(true))
+    assert(singleton(List(t2)).equals(true))
+    assert(singleton(List(t1, t1)).equals(false))
+    assert(singleton(List(t1, t2)).equals(false))
+    assert(singleton(List()).equals(false))
+
+  }
+
+
+  test("combine of some leaf list") {
+    val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
+    assert(combine(leaflist) === List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
+  }
+
+
+  test("decode and encode a very short text should be identity") {
+    new TestTrees {
+      assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
+    }
+  }
+
+}
